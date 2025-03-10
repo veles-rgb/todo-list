b29ad36205a765f94ea40d1815785cd38e1d0485
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-03-10 15:33:48 -0400

    Edit default tasks to include new priority options

--- src/js/index.js
+++ src/js/index.js
@@ -7,10 +7,10 @@ import { renderTasks, renderTodos, renderInfo } from "./modules/dom.js";
 
 function addDefault() {
     const defaultTodo1 = TodoList.addTodo("Make A Todo App", "Incomplete");
-    defaultTodo1.tasks.push(new Task("Do this", "Description", "2025-03-10T15:30", "Priority"));
-    defaultTodo1.tasks.push(new Task("Do that", "Description", "2025-12-25T08:15", "Priority"));
+    defaultTodo1.tasks.push(new Task("Do this", "Description", "2025-03-10T15:30", "!!!"));
+    defaultTodo1.tasks.push(new Task("Do that", "Description", "2025-12-25T08:15", "!!"));
     const defaultTodo2 = TodoList.addTodo("Test", "Incomplete");
-    defaultTodo2.tasks.push(new Task("Title", "Description", "2026-07-04T19:45", "Priority"));
+    defaultTodo2.tasks.push(new Task("Title", "Description", "2026-07-04T19:45", "!"));
     defaultTodo2.tasks[0].notes = "test"
 };
 
