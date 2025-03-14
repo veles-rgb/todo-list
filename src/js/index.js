da5063a9cb95fbf05c3ec5a7bedcc35019c4d6d5
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-03-14 12:54:49 -0400

    Remove unnecessary imports

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,6 @@
 // index.js
 import "../styles/reset.css"
 import "../styles/styles.css";
-import TodoList from "./modules/todo.js";
-import Task from "./modules/task.js";
 import { renderTasks, renderTodos, renderInfo } from "./modules/dom.js";
 import { getStoredTodos } from "./modules/storage.js";
 
